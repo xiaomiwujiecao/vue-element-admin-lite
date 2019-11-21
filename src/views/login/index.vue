ad8805a211dafd7862e974482c0fba32e07618e4
huhongyun <xiaomiwujiecao@gmail.com>
2019-11-21 14:56:39 +0800

    添加自定义样式 精简修改登录页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">项目管理系统</h3>
       </div>
 
       <el-form-item prop="username">
@@ -45,9 +45,9 @@
         </el-form-item>
       </el-tooltip>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
 
-      <div style="position:relative">
+      <!--<div style="position:relative">
         <div class="tips">
           <span>Username : admin</span>
           <span>Password : any</span>
@@ -60,16 +60,16 @@
         <el-button class="thirdparty-button" type="primary" @click="showDialog=true">
           Or connect with
         </el-button>
-      </div>
+      </div>-->
     </el-form>
 
-    <el-dialog title="Or connect with" :visible.sync="showDialog">
-      Can not be simulated on local, so please combine you own business simulation! ! !
-      <br>
-      <br>
-      <br>
-      <social-sign />
-    </el-dialog>
+<!--    <el-dialog title="Or connect with" :visible.sync="showDialog">-->
+<!--      Can not be simulated on local, so please combine you own business simulation! ! !-->
+<!--      <br>-->
+<!--      <br>-->
+<!--      <br>-->
+<!--      <social-sign />-->
+<!--    </el-dialog>-->
   </div>
 </template>
 
